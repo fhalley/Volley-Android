40ad2653135e87ef3868aba93941bfa8e4d8f50a
Edward <mrandmrs_halley@yahoo.com>
2015-01-22 15:12:42 +0700

    First Commit for Init

---
+++ volley/platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\phonegap\volley\platforms\android\ant-build\AndroidManifest.xml : \
+C:\phonegap\volley\platforms\android\AndroidManifest.xml \
---
+++ volley/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\phonegap\volley\platforms\android\ant-build\CordovaApp-debug-unaligned.apk : \
+C:\phonegap\volley\platforms\android\ant-build\CordovaApp.ap_ \
+C:\phonegap\volley\platforms\android\ant-build\classes.dex \
+C:\phonegap\volley\platforms\android\CordovaLib\ant-build\classes.jar \
