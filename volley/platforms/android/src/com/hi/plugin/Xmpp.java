96e725544bdc11923438abfe065347fb9b6e6f36
Edward <mrandmrs_halley@yahoo.com>
2015-02-01 02:51:35 +0700

    Add Cordova Xmpp Init Plugins
    Add Cordova Xmpp Init Plugins

--- volley/platforms/android/src/com/hi/plugin/Xmpp.java
+++ volley/platforms/android/src/com/hi/plugin/Xmpp.java
@@ -2,28 +2,58 @@ package com.hi.plugin;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class Xmpp extends CordovaPlugin {
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("echo")) {
-            String message = args.getString(0);
-            this.echo(message, callbackContext);
-            return true;
-        }
-        return false;
-    }
 
-    private void echo(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+	public static final String SERVER_HOST = "66.208.118.221";
+	public static final int SERVER_PORT = 5222;
+	public static final String SERVICE = "volley.com ";
+	public static final String ACTION_INIT = "xmpp_init";
+	public static final String ACTION_REGISTER = "xmpp_register"; 
+	public static final String ACTION_SEND_MSG = "sendmsg";
+	public static final String ACTION_ADD_CONTACT = "xmpp_add_contact";
+	public static final String ACTION_VERIFY = "xmpp_verify";
+	public static final String ACTION_DAFTAR = "xmpp_daftar";
+	public static final String ACTION_GET_PROFILE = "xmpp_get_profile";
+	public static final String ACTION_GET_ROSTER = "xmpp_get_roster";
+	public final String VERIFY_CODE = "15367";
+	public static final String CONS_PASSWORD = "asdf";
+		
+	
+	
+    @Override
+    public boolean execute(final String action, final JSONArray args, final CallbackContext callbackContext) {
+    	Log.i("XmppPlugin.Info","Open Excute");
+    	if(ACTION_INIT.equals(action)||ACTION_REGISTER.equals(action)
+    			||ACTION_ADD_CONTACT.equals(action)||ACTION_SEND_MSG.equals(action)||ACTION_DAFTAR.equals(action)
+    			||ACTION_GET_PROFILE.equals(action)||ACTION_VERIFY.equals(action)||ACTION_GET_ROSTER.equals(action))
+    	{
+        	//Thread t = new Thread(new Runnable() {
+    		cordova.getThreadPool().execute(new Runnable() {
+    			//@Override
+    			public void run() {
+    				Log.i("XmppPlugin.Info","Open Thread");
+    				// TODO Auto-generated method stub
+    				if(ACTION_INIT.equals(action)){
+    					Log.i("XmppPlugin.Info","Open Init");
+    					String hi="ASD";
+						callbackContext.success(hi);
+    				}
+    			}
+    		});
+    				return true;
+    	}
+        return false;
     }
 }
--- volley/www/js/index.js
+++ volley/www/js/index.js
@@ -1,51 +1,23 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+function init() {
+    console.log('Initializing......');
+    function callNativePlugin(callback) {
+        cordova.exec(function(result) {
+            callback(null, result);
+        }, function(result) {
+            callback("myerror");
+        }, "Xmpp", "xmpp_init", [])
     }
-};
+    ;
 
-app.initialize();
+    var callbackFunction = function(err, result) {
+        if (err) {
+            //window.location.replace("register.html");
+            console.log("Nt Authorized, Assuming need to register");
+        } else {
+            console.log("success," + result);
+        }
+        ;
+    }
+callNativePlugin(callbackFunction);
+}
+document.addEventListener("deviceready", init, false);
