8208bc879f1910abf4faf22f4e5b3bf718ea58ee
edvard_cs <edvard_122@hotmail.com>
2015-02-13 16:23:19 -0800

    Add validate-code.js
    Call native code to verificate the code

---
+++ volley/www/js/validate-code.js
@@ -0,0 +1,30 @@
+function register() {
+    callNativePlugin(callbackFunction);
+    console.log("Final");
+    //alert("Pico el bagre");
+}
+var el = document.getElementById("verificate");
+el.addEventListener("click", register, false);
+
+function callNativePlugin(callback) {
+        var code = $("#code").val();
+        cordova.exec(function(result) {
+            callback(null, result);
+        }, function(result) {
+            callback("myerror");
+        }, "Xmpp", "xmpp_verify", [code])
+    }
+;
+
+var callbackFunction = function(err, result) {
+    console.log("Waiting ToCallback");
+    if (err) {
+        window.location.replace("register.html");
+        alert("Error");
+    } else {
+        console.log("Ok for verification.");
+        console.log("success," + result);
+        alert(result);
+        //window.location.replace("verification.html");
+    }
+ };
